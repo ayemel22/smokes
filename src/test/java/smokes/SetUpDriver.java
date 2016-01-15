1c6f27aa06284221c445b5372fbe766abe687210
ayem22 <ayemelyanenko@chegg.com>
2016-01-15 14:22:13 -0800

    come on now

--- src/test/java/smokes/SetUpDriver.java
+++ src/test/java/smokes/SetUpDriver.java
@@ -21,7 +21,7 @@ public class SetUpDriver {
 		((DesiredCapabilities) caps).setCapability("takesScreenshot", true);
 		((DesiredCapabilities) caps).setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, cli_args);
 		((DesiredCapabilities) caps).setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
-				"/Users/ayemelyanenko/Desktop/phantomjs");
+				"/usr/local/bin/phantomjs");
 		driver = new PhantomJSDriver(caps);
 		driver.manage().window().maximize();
 		driver.navigate().to("https://rc.live.test.cheggnet.com/tutors/");
