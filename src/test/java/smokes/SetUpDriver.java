43997fd38493283cfa73d495ab71581d4ba1a91b
ayem22 <ayemelyanenko@chegg.com>
2016-01-15 13:56:42 -0800

    Well

--- src/test/java/smokes/SetUpDriver.java
+++ src/test/java/smokes/SetUpDriver.java
@@ -4,6 +4,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -14,14 +15,7 @@ public class SetUpDriver {
 
 	@Before
 	public void setUp() {
-		String[] cli_args = new String[]{ "--ignore-ssl-errors=true" };
-		Capabilities caps = new DesiredCapabilities();
-		((DesiredCapabilities) caps).setJavascriptEnabled(true);
-		((DesiredCapabilities) caps).setCapability("takesScreenshot", true);
-		((DesiredCapabilities) caps).setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, cli_args);
-		((DesiredCapabilities) caps).setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
-				"/Users/ayemelyanenko/Desktop/phantomjs");
-		driver = new PhantomJSDriver(caps);
+		driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.navigate().to("https://rc.live.test.cheggnet.com/tutors/");
 	}
