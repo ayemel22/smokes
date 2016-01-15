8c29abe76b7533ed9eb05cee00e02946bb3f4900
ayem22 <ayemelyanenko@chegg.com>
2016-01-15 14:09:35 -0800

    yes

--- src/test/java/smokes/SetUpDriver.java
+++ src/test/java/smokes/SetUpDriver.java
@@ -15,7 +15,14 @@ public class SetUpDriver {
 
 	@Before
 	public void setUp() {
-		driver = new FirefoxDriver();
+		String[] cli_args = new String[]{ "--ignore-ssl-errors=true" };
+		Capabilities caps = new DesiredCapabilities();
+		((DesiredCapabilities) caps).setJavascriptEnabled(true);
+		((DesiredCapabilities) caps).setCapability("takesScreenshot", true);
+		((DesiredCapabilities) caps).setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, cli_args);
+		((DesiredCapabilities) caps).setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
+				"/Users/ayemelyanenko/Desktop/phantomjs");
+		driver = new PhantomJSDriver(caps);
 		driver.manage().window().maximize();
 		driver.navigate().to("https://rc.live.test.cheggnet.com/tutors/");
 	}
