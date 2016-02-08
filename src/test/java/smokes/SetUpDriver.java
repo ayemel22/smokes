7e387d1558995f302d0edd4af2424e871d28aaa4
ayem22 <ayemelyanenko@chegg.com>
2016-02-08 11:01:00 -0800

    change back url

--- src/test/java/smokes/SetUpDriver.java
+++ src/test/java/smokes/SetUpDriver.java
@@ -1,17 +1,9 @@
 package smokes;
 
-import java.util.concurrent.TimeUnit;
-
 import org.junit.After;
 import org.junit.Before;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
-import org.openqa.selenium.phantomjs.PhantomJSDriverService;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class SetUpDriver {
 
@@ -21,7 +13,7 @@ public class SetUpDriver {
 	public void setUp() {
 		driver = new FirefoxDriver();
 		driver.manage().window().maximize();
-		driver.navigate().to("https://www.chegg.com/tutors/");
+		driver.navigate().to("https://rc.live.test.cheggnet.com/tutors/");
 	}
 
 	@After
